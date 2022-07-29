57fe5fff00baf245a12ae53fb8b7b0f3cee17a60
TakayukiKomada <Takakoma.79@gmail.com>
2022-07-29 09:54:09 +0900

    phpに変更途中まで

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <?php include_once __DIR__ . '/_head.html' ?>
+    <title>Rentaler</title>
+</head>
+<body>
+<?php include_once __DIR__ . '/_header.html' ?>
+    
+    <div class="home-content wrapper">
+        <h2>Rentaler</h2>
+        <p>Rentalerは誰でも自由に働けるお仕事マッチングアプリです。</p>
+        <a class="button" href="worklogin.html">Rentalerとしてログインする</a>
+        <a class="button" href="login.html">ログインして仕事を依頼する</a>
+    </div>
+    <?php include_once __DIR__ . '/_footer.html' ?>
+    
+    </body>
+</html>
